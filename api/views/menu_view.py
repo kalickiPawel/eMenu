808733a0ee812af2a7ba6199551e606e5bf4a566
Paweł <kalicki.paul@icloud.com>
2021-02-28 00:14:15 +0100

    :lock: Update MenuViewSet permission

--- api/views/menu_view.py
+++ api/views/menu_view.py
@@ -7,17 +7,24 @@ from api.models import Menu
 
 
 class MenuViewSet(viewsets.ModelViewSet):
-    queryset = Menu.objects.filter(dishes__isnull=False)
+    """
+    This ViewSet automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    The queryset is setup with permission
+    """
     serializer_class = MenuSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['name', 'created_at', 'updated_at']
 
-    @method_decorator(login_required)
-    def create(self, request, *args, **kwargs):
-        return super(MenuViewSet, self).create(request, *args, **kwargs)
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Menu.objects.all()
+        else:
+            return Menu.objects.filter(dishes__isnull=False)
 
-    def list(self, request, *args, **kwargs):
-        return super(MenuViewSet, self).list(request, *args, **kwargs)
+    def perform_create(self, serializer):
+        serializer.save()
 
     # def get_queryset(self):
     #     sort = self.request.GET.get('sort_by')
