571beab73cdf1a9274440e806657c7a0742e78e3
Paweł <kalicki.paul@icloud.com>
2021-02-28 17:20:38 +0100

    :sparkles: Added ordering mechanism

--- api/views/menu_view.py
+++ api/views/menu_view.py
@@ -1,5 +1,6 @@
-# from django.db.models import Count
+from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 from rest_framework import viewsets
 
 from api.serializers import MenuSerializer
@@ -15,20 +16,27 @@ class MenuViewSet(viewsets.ModelViewSet):
     GET, POST, PUT and DELETE actions.
     """
     serializer_class = MenuSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ['name', 'created_at', 'updated_at']
+    ordering_fields = ['name']
 
     def get_queryset(self):
+        queryset = Menu.objects
+        query_filter = self.request.query_params.get('ordering', None)
+
+        allow_filters = ['dishes_count']
+        allow_filters += [f'-{el}' for el in allow_filters]
+
+        if query_filter in allow_filters:
+            if query_filter == r'dishes_count':
+                queryset = queryset.annotate(
+                    dishes_count=Count('dishes')
+                ).order_by(query_filter)
+
         if self.request.user.is_authenticated:
-            return Menu.objects.all()
+            return queryset
         else:
-            return Menu.objects.filter(dishes__isnull=False)
+            return queryset.annotate(dishes_count=Count('dishes')).filter(dishes_count__gt=0)
 
     def perform_create(self, serializer):
         serializer.save()
-
-    # return Menu.objects.filter(dishes__isnull=False).annotate(
-    #     number_of_dishes=Count('dishes')
-    # ).order_by('-number_of_dishes')
-
-    # return Menu.objects.filter(dishes__isnull=False).order_by('name')
